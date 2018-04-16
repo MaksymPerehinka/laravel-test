dc8413a6ab98a32ad0675de8b0b2dfd9848fa415
MaksymPerehinka <maksym.perehinka@gmail.com>
2018-04-16 06:35:17 +0300

    fix localization text

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,7 +15,7 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-    'text_successful_registration' => 'Thank you for registration! Confirmation link was sent to your email address',
+    'text_successful_registration' => 'Thank you for registration! Confirmation link has been sent to your email address',
     'text_successful_confirmation_link_sent' => 'The confirmation link has been successfully send to your email!',
     'error_confirmation' => 'You have to confirm your registration before you will be able to sign in. <a href=":uri">Send confirmation link</a>'
 ];
